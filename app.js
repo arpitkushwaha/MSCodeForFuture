a9849a50e10dc16c0d6a4bddf3c7ee02b305f891
Arpit Kushwaha <arpitkushwah01@gmail.com>
2020-03-23 00:27:06 +0530

    Added Post Routes

--- app.js
+++ app.js
@@ -10,17 +10,43 @@ app.use(express.static(__dirname+"/public"));
   {
       res.render("home");
   });
+
+
 // ======================== LOGIN ========================
 app.get("/signin",function(req,res)
 {
     res.render("login");
 });
 
+app.post('/signin',function(req,res){
+
+    var email = req.body.email;
+    var password = req.body.pass;
+    console.log("Email : " + email);
+    console.log("Password : " + password);
+
+    if(email !="" && password !="")
+        res.render("dashboard");
+  });
+
+
 // ======================== REGISTER ========================
 app.get("/register",function(req,res)
 {
     res.render("register");
+    
 });
+
+app.post('/register',function(req,res){
+
+    var firstname = req.body.f_name;
+    var lastname = req.body.l_name;
+    var email = req.body.email;
+    var password = req.body.pass;
+
+    if(firstname !="" && lastname !="" && email !="" && password !="")
+        res.render("login");
+  });
 
 // ======================== MAIN DASHBOARD ========================
 app.get("/dashboard",function(req,res)
