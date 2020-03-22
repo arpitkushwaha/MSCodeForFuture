d41ae2a1552710932eba84a5e99c61214e9d4985
Rexbec <pratikkverma5@gmail.com>
2020-03-22 22:32:03 +0530

    added pages

--- app.js
+++ app.js
@@ -6,9 +6,32 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine",'ejs');
 app.use(express.static(__dirname+"/public"));
 
-app.get("/",function(req,res)
+  app.get("/",function(req,res)
+  {
+      res.render("home");
+  });
+// ======================== LOGIN ========================
+app.get("/signin",function(req,res)
 {
-    res.render("home");
+    res.render("login");
+});
+
+// ======================== REGISTER ========================
+app.get("/register",function(req,res)
+{
+    res.render("register");
+});
+
+// ======================== MAIN DASHBOARD ========================
+app.get("/dashboard",function(req,res)
+{
+    res.render("dashboard");
+});
+
+// ======================== EMPLOYEE DASHBOARD ========================
+app.get("/employee",function(req,res)
+{
+    res.render("employee");
 });
 
 app.listen(process.env.PORT || 3000, function(req,res)
